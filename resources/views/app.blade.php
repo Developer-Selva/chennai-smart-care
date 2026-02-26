02d61f1816c76b96e9f1cacacb26a637b19694b0
Developer-Selva <selvakumar.r.fsd@gmail.com>
2026-02-26 12:29:46 +0530

    Favicon update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,9 +23,9 @@
     })(window,document,'script','dataLayer','GTM-TF7JP7GT');</script>
     <!-- End Google Tag Manager -->
 
-    <link rel="icon" type="image/png" href="images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="https://www.chennaismartcare.com/images/favicon/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="images/favicon/favicon.svg" />
-    <link rel="shortcut icon" href="images/favicon/favicon.ico" />
+    <link rel="icon" href="https://www.chennaismartcare.com/images/favicon/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
     <link rel="manifest" href="images/favicon/site.webmanifest" />
 
