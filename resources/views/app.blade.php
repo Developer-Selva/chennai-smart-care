d17ece427d7d9d3567c8704461f1c001cca9cd88
Developer-Selva <selvakumar.r.fsd@gmail.com>
2026-02-28 17:26:57 +0530

    Google tag manager update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,23 +16,12 @@
     </script> -->
 
     <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TF7JP7GT');</script> -->
+    })(window,document,'script','dataLayer','GTM-TF7JP7GT');</script>
     <!-- End Google Tag Manager -->
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H77HGW29PN">
-    </script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-H77HGW29PN');
-    </script>
 
     <link rel="icon" type="image/png" href="https://www.chennaismartcare.com/images/favicon/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="images/favicon/favicon.svg" />
